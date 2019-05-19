7ee9f1972823452af1c57dbb4eceeb39b5795529
Katie Macaulay <katiemmacaulay@gmail.com>
2019-05-19 14:46:02 -0500

    playing with redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,27 @@
-export function setSpecialText(txt){
-  return {
-    type:"SET_SPECIAL_TEXT",
-    value:txt
+// export function setSpecialText(txt){
+//   return {
+//     type:"SET_SPECIAL_TEXT",
+//     value:txt
+//   }
+// }
+
+const txt = {
+
+  arr:[]
+}
+
+export function setSpecialText(state = txt, action){
+  switch (action.type){
+     case 'SET_SPECIAL_TEXT' :
+       return { 
+          ...state,
+          arr:[...state.arr, action.newItem]
+     }
+
+     default:return state
   }
 }
+
 
 export function searchText(txt){
   return {
--- src/containers/SpecialTextBoxContainer.js
+++ src/containers/SpecialTextBoxContainer.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import {setSpecialText} from "../actions";
 import SpecialTextBox from "../components/SpecialTextBox";
+import { bindActionCreators } from "redux"
 
 
-const mapDispatchToProps = {
-   set:setSpecialText
-  }
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    set: bindActionCreators(setSpecialText, dispatch)
+  } 
+}
 
 export default connect(null,mapDispatchToProps)(SpecialTextBox);
